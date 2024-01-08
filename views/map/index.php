3e16bf1d6a242f5169d46279f5d5bafaafe6ae6a
Mariam Manukyan <mariammanukyan27012002@gmail.com>
2024-01-08 12:26:35 -0800

    location setInterval(1000)

--- views/map/index.php
+++ views/map/index.php
@@ -32,7 +32,64 @@ $this->params['date_tab'] = $date_tab;
             <input id="myLocalDate" class="fil-input form-control valuemap" type="datetime-local" name="date">
         </div>
     </div>
-    <div id="map"></div>
+    <div id="map">
+
+    </div>
+    <script>
+        const x = document.getElementById("map");
+
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            x.innerHTML = "Latitude: " + position.coords.latitude +
+                "<br>Longitude: " + position.coords.longitude;
+        }
+        setInterval(getLocation, 1000);
+        getLocation();
+    </script>
+
+<!--    <script>-->
+<!--        ymaps.ready(init);-->
+<!---->
+<!--        function init() {-->
+<!--            var map = new ymaps.Map("map", {-->
+<!--                center: [0, 0],-->
+<!--                zoom: 3-->
+<!--            });-->
+<!---->
+<!--            var placemark = new ymaps.Placemark([0, 0], {-->
+<!--                balloonContent: "Your Location"-->
+<!--            });-->
+<!---->
+<!--            map.geoObjects.add(placemark);-->
+<!---->
+<!--            function updateLocation() {-->
+<!--                navigator.geolocation.getCurrentPosition(-->
+<!--                    function(position) {-->
+<!--                        var userLocation = [position.coords.latitude, position.coords.longitude];-->
+<!---->
+<!--                        placemark.geometry.setCoordinates(userLocation);-->
+<!--                        map.setCenter(userLocation);-->
+<!--                        console.log(userLocation)-->
+<!--                    },-->
+<!--                    // function(error) {-->
+<!--                    //     console.error('Error getting user location:', error);-->
+<!--                    // }-->
+<!--                );-->
+<!--            }-->
+<!---->
+<!--            setInterval(updateLocation, 1000);-->
+<!---->
+<!--            updateLocation();-->
+<!--        }-->
+<!--    </script>-->
+
 </body>
 <style>
     body, #map {
